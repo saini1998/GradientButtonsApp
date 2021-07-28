9d5d2d3e0ab3d17ac5eaf4bee52c611efb5f370d
saini1998 <sainiaaryaman1998@gmail.com>
2021-07-28 19:12:26 +0100

    Initial Commit

---
+++ GradientButtons/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GradientButtons
+//
+//  Created by Aaryaman Saini on 28/07/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GradientButtons/GradientButtonsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GradientButtonsApp.swift
+//  GradientButtons
+//
+//  Created by Aaryaman Saini on 28/07/21.
+//
+
+import SwiftUI
+
+@main
+struct GradientButtonsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
